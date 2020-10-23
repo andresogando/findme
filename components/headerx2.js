46e33c35d46539252a15216cf2d7ad97300dc73d
andress <Andres@Andress-MacBook-Pro.local>
2020-10-23 16:32:23 -0400

    added modal functionality to the header with DEMO forms for report a missing person

--- components/headerx2.js
+++ components/headerx2.js
@@ -2,11 +2,55 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
+
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
+import  {Modal, Form, Button} from 'react-bootstrap';
+
+
+function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Report a Missing Person and  we handle from here! 
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
 
 
 
@@ -30,14 +74,9 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Header(props) {
-    const sections = [
-        { title: 'Report Missing', url: '#' },
-        { title: 'Who we are?', url: '#' },
-        { title: 'FAQS', url: '#' },
-        { title: 'About', url: '#' },
-        { title: 'Volunteer', url: '#' },
-        { title: 'Events', url: '#' },
-      ];
+  const [modalShow, setModalShow] = React.useState(false);
+
+    
 
   const classes = useStyles();
     const {  title } = props;
@@ -45,7 +84,7 @@ export default function Header(props) {
   return (
     <React.Fragment>
       <Toolbar className={classes.toolbar}>
-        <Button size="small"> Subscribe </Button>
+        <Button variant="outlined" size="small"> Subscribe </Button>
         <Typography
           component="h2"
           variant="h5"
@@ -63,20 +102,61 @@ export default function Header(props) {
           Sign up
         </Button>
       </Toolbar>
-      <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
-        {sections.map((section) => (
+        <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
+       
           <Link
             color="inherit"
             noWrap
-            key={section.title}
             variant="body2"
-            href={section.url}
+            className={classes.toolbarLink}>
+                <div onClick={() => setModalShow(true)}> Missing/Kidnappings Persons? </div>
+            </Link>
+            <Link
+            color="inherit"
+            noWrap
+            variant="body2"
+            className={classes.toolbarLink}>
+                <div> Who we are? </div>
+            </Link>
+
+            <Link
+            color="inherit"
+            noWrap
+            variant="body2"
+            className={classes.toolbarLink}>
+                <div> FAQS </div>
+            </Link>
+
+            <Link
+            color="inherit"
+            noWrap
+            variant="body2"
+            className={classes.toolbarLink}>
+                <div> About </div>
+            </Link>
+
+            <Link
+            color="inherit"
+            noWrap
+            variant="body2"
+            className={classes.toolbarLink}>
+                <div> Volunteer </div>
+            </Link>
+
+            <Link
+            color="inherit"
+            noWrap
+            variant="body2"
             className={classes.toolbarLink}
-          >
-            {section.title}
-          </Link>
-        ))}
+            href="/">
+                <div> Events </div>
+            </Link> 
+          
       </Toolbar>
+      <MyVerticallyCenteredModal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+        />
     </React.Fragment>
   );
 }
--- pages/b/showroom.js
+++ pages/b/showroom.js
@@ -3,11 +3,25 @@ import Headerx2 from '../../components/headerx2';
 import Layout from '../../components/layout';
 
 
+//const [modalShow, setModalShow] = React.useState(false);
+/**
+ * <Button variant="primary" onClick={() => setModalShow(true)}>
+            Launch vertically centered modal
+        </Button>
 
+        <MyVerticallyCenteredModal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+        />
+ */
 export default function Showroom(){
+    
+
     return(
         <Layout>
-        <Headerx2 />
+
+        
+    
         
 
         </Layout>
